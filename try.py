20bdea792231d7494e4e0eea301f69ea3a6293c9
EL-HADDADI <112482715+EL-HADDADI@users.noreply.github.com>
2022-09-20 09:42:35 +0100

    Delete try.py

--- try.py
+++
@@ -1,7 +0,0 @@
-from collections import ChainMap
-a  = {"name":'james', "age":20,"address":'thomas salako street,ogba',"phone":37822}
-b  = {"name":'olu', "age":36,"address":'oke ira street,ogba',"phone":98876}
-c  = {"name":'kate', "age":80,"address":'kington kete street,ogba',"phone":174528}
-
-d = ChainMap(a,b,c)
-print(d)
